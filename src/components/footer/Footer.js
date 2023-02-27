7ce464567e0145c01e395a37b5affe3f1a72de24
Amandeep Singh <96967202+ASA-HELPER@users.noreply.github.com>
2023-02-27 14:39:01 +0530

    Entertainment-4U app completed successfully!!!

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
+import "./style.scss";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <ContentWrapper>
+                <ul className="menuItems">
+                    <li className="menuItem">Terms Of Use</li>
+                    <li className="menuItem">Privacy-Policy</li>
+                    <li className="menuItem">About</li>
+                    <li className="menuItem">Blog</li>
+                    <li className="menuItem">FAQ</li>
+                </ul>
+                <div className="infoText">
+                    Entertainment-4U
+                </div>
+                <h5>&copy;2023 | All Rights Reserved</h5>
+            </ContentWrapper>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/pages/home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import HeroBanner from './heroBanner/HeroBanner';
+import Popular from './popular/Popular';
+import "./style.scss";
+import TopRated from './topRated/TopRated';
+import Trending from './trending/Trending';
+const Home = () => {
+    return (
+        <div className='homePage'>
+            <HeroBanner />
+            <Trending />
+            <Popular />
+            <TopRated />
+        </div>
+    )
+}
+
+export default Home
